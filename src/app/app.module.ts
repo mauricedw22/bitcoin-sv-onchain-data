3fc74368d5f990c7475482971a47fcd46fc114b5
Maurice WALKER <mauricedw22@gmail.com>
2022-01-17 00:09:02 -0500

    added charts api service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +8,7 @@ import { AppComponent } from './app.component';
 import { ChartModule } from 'primeng/chart';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ChartsComponent } from './charts/charts.component';
+import { ChartsApiService } from './charts/services/charts-api.service';
 
 @NgModule({
   declarations: [
@@ -19,7 +21,7 @@ import { ChartsComponent } from './charts/charts.component';
     AppRoutingModule,
     ChartModule
   ],
-  providers: [],
+  providers: [ChartsApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/charts/services/charts-api.service.ts
+++ src/app/charts/services/charts-api.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChartsApiService {
 
-  constructor() { }
+  url: string = 'https://api.whatsonchain.com/v1/bsv/main/chain/info';
+  headers = new HttpHeaders().set('Content-Type', 'application/json');
+
+  data: any;
+
+  constructor(private http: HttpClient) { }
+
+  getChainData() {
+    this.data = this.http.get(this.url);
+    console.log(this.data);
+    // return this.http.get(this.url);
+  }
+
 }
